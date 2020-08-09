438c4cd4b2c19d358176715e62f11779d76e865a
Tomas <michalica.t@gmail.com>
2020-08-09 19:13:11 +0200

    listing words and suggestions

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,14 +1,35 @@
 import React from 'react';
 import BaseTemplate from '../../templates/BaseTemplate/BaseTemplate';
 import Phone from '../../components/organizms/Phone';
+import WordsContainer from '../../containers/WordsContainer';
+import Words from '../../components/atoms/Words';
+import SuggestionsContainer from '../../containers/SuggestionsContainer';
 
 const Main = () => {
   return (
     <BaseTemplate
       render={() => (
-        <>
-          <Phone />
-        </>
+        <SuggestionsContainer
+          render={({ suggestions, getSuggestions }) => (
+            <WordsContainer
+              render={({ words, getWords }) => {
+                return (
+                  <>
+                    <Phone
+                      onType={(newValue: string) => {
+                        getWords(newValue);
+                        getSuggestions(newValue);
+                      }}
+                    />
+                    {suggestions.map((w, num) => (
+                      <Words key={w}>{w}</Words>
+                    ))}
+                  </>
+                );
+              }}
+            />
+          )}
+        />
       )}
     />
   );
