94e1ea08f1713da75c512895e11a609cbd6f8cd2
Tomas <michalica.t@gmail.com>
2020-08-11 09:13:14 +0200

    adding Button

---
+++ src/components/atoms/Button/Button.tsx
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+
+const Button = styled.button`
+  background: none;
+  border: none;
+  color: white;
+  background: #00a991;
+  padding: 15px;
+  cursor: pointer;
+  border: 1px solid white;
+`;
+export default Button;
