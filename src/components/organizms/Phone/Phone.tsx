480cc1165b43124f7e155fa8471b5f0400d036fb
Tomas <michalica.t@gmail.com>
2020-08-11 09:21:06 +0200

    adding keyboard to phone, generating random color, editing styles og Phone

--- src/components/organizms/Phone/Phone.tsx
+++ src/components/organizms/Phone/Phone.tsx
@@ -1,11 +1,50 @@
-import React, { ChangeEvent } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import styled from '@emotion/styled';
 import DefaultInput from '../../molecules/DefaultInput';
 import Suggestions from '../../molecules/Suggestions';
 import Words from '../../molecules/Words';
+import Button from '../../atoms/Button';
 
 export const defaultProps = {
-  Wrapper: styled.div``,
+  Wrapper: styled.div`
+    border: 2px solid #00a991;
+    max-width: 300px;
+    height: 400px;
+    border-radius: 20px;
+    background: ${(props) => props.color};
+  `,
+  Display: styled.div`
+    border: 1px solid #cdcdcd;
+    margin: 15px;
+    display: flex;
+    flex-direction: column;
+    border-radius: 10px;
+    align-items: center;
+    padding: 10px;
+  `,
+  SuggestionsWrapper: styled.div`
+    height: 120px;
+  `,
+  WordsWrapper: styled.div`
+    height: 100px;
+    display: flex;
+  `,
+  Row: styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+  `,
+  Keyboard: styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  `,
+  Speaker: styled.div`
+    border: 2px solid black;
+    width: 20px;
+    margin: 0px auto;
+    margin-top: 10px;
+  `,
   Input: DefaultInput,
   SuggestionComponent: Suggestions,
   WordsComponent: Words,
@@ -16,37 +55,139 @@ type Props = Readonly<
   suggestions: string[];
   words: string[];
   onType?(newValue: string): void;
+  value?: string;
+  setValue?: (value: string) => void;
+  color?: string;
+  setColor?: (value: string) => void;
+  add?: (addValue: string, props: Partial<Props>) => void;
+  getRandomColor?: () => string;
 }
 >;
 type Children = (props: Props) => JSX.Element;
 
 const Phone = ({ children, ...props }: { children: Children } & Props) => {
-  return children(props);
+  const [value, setValue] = useState('');
+  const [color, setColor] = useState('white');
+
+  return children({ color, setColor, value, setValue, ...props });
 };
 
 Phone.defaultProps = {
   ...defaultProps,
+  getRandomColor: (): string => {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  },
+  add: (
+    addValue: string,
+    { value, setValue, onType, setColor, getRandomColor }: Partial<Props>
+  ): void => {
+    setValue!(`${value + addValue}`);
+    onType?.(`${value + addValue}`);
+    setColor!(getRandomColor!());
+  },
   children: ({
     Wrapper,
     Input,
     onType,
     SuggestionComponent,
-    WordsComponent,
-    words,
     suggestions,
+    SuggestionsWrapper,
+    Display,
+    Keyboard,
+    Row,
+    setValue,
+    value,
+    Speaker,
+    add,
+    getRandomColor,
+    setColor,
+    color,
   }: Props) => (
-    <Wrapper>
-      <>
-        <WordsComponent words={words} />
-        <Input
-          onChange={(e: ChangeEvent<HTMLInputElement>, { setValue }) => {
-            const { value } = e.target;
-            setValue?.(value);
-            onType?.(value);
-          }}
-        />
-        <SuggestionComponent suggestions={suggestions} />
-      </>
+    <Wrapper color={color}>
+      <Speaker />
+      <Display>
+        <>
+          <div>Try 236:</div>
+          <SuggestionsWrapper>
+            <SuggestionComponent
+              suggestions={suggestions}
+              numberOfSuggestions={3}
+            />
+          </SuggestionsWrapper>
+          <Input
+            onChange={(e: ChangeEvent<HTMLInputElement>) => {
+              const eValue = e.target.value;
+              setValue?.(eValue);
+              onType?.(eValue);
+              setColor?.(getRandomColor!());
+            }}
+            value={value}
+          />
+        </>
+      </Display>
+
+      <Keyboard>
+        <Row>
+          <Button disabled>1</Button>
+          <Button
+            onClick={() =>
+              add!('2', { value, onType, setValue, setColor, getRandomColor })}
+          >
+            2
+          </Button>
+          <Button
+            onClick={() =>
+              add!('3', { value, onType, setValue, setColor, getRandomColor })}
+          >
+            3
+          </Button>
+        </Row>
+        <Row>
+          <Button
+            onClick={() =>
+              add!('4', { value, onType, setValue, setColor, getRandomColor })}
+          >
+            4
+          </Button>
+          <Button
+            onClick={() =>
+              add!('5', { value, onType, setValue, setColor, getRandomColor })}
+          >
+            5
+          </Button>
+          <Button
+            onClick={() =>
+              add!('6', { value, onType, setValue, setColor, getRandomColor })}
+          >
+            6
+          </Button>
+        </Row>
+        <Row>
+          <Button
+            onClick={() =>
+              add!('7', { value, onType, setValue, setColor, getRandomColor })}
+          >
+            7
+          </Button>
+          <Button
+            onClick={() =>
+              add!('8', { value, onType, setValue, setColor, getRandomColor })}
+          >
+            8
+          </Button>
+          <Button
+            onClick={() =>
+              add!('9', { value, onType, setValue, setColor, getRandomColor })}
+          >
+            9
+          </Button>
+        </Row>
+      </Keyboard>
     </Wrapper>
   ),
 };
--- src/components/organizms/Phone/index.test.tsx
+++ src/components/organizms/Phone/index.test.tsx
@@ -1,19 +1,81 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, RenderResult, fireEvent } from '@testing-library/react';
 import Phone from './Phone';
 
-describe('snapshot tests', () => {
+describe('snapshot testing', () => {
   it('testing rendered tree', () => {
     const { container } = render(<Phone suggestions={[]} words={[]} />);
     expect(container).toMatchSnapshot();
   });
+});
 
-  it('testing rendered tree', () => {
-    const { getByText, getByLabelText } = render(
+describe('assertion tests', () => {
+  let utils: RenderResult;
+
+  beforeEach(() => {
+    utils = render(
       <Phone suggestions={['suggestion value']} words={['words value']} />
     );
+  });
+  it('has suggestion values and input', () => {
+    const { getByText, getByLabelText } = utils;
+
     expect(getByText('suggestion value')).toBeInTheDocument();
-    expect(getByText('words value')).toBeInTheDocument();
     expect(getByLabelText('numbers')).toBeInTheDocument();
+  });
+  it('has suggestion values and input', () => {
+    const { container } = utils;
+    const buttons = container.querySelectorAll('button');
+
+    expect(buttons.length).toBe(9);
+  });
+  it('has label', () => {
+    const { getByText } = utils;
+
+    expect(getByText('Try 236:')).toBeInTheDocument();
+  });
+  it('click button change input value', () => {
+    const { getByText, getByLabelText } = utils;
+
+    const button = getByText('2') as HTMLButtonElement;
+    const input: HTMLInputElement = getByLabelText(
+      'numbers'
+    ) as HTMLInputElement;
+
+    fireEvent.click(button);
+
+    expect(input.value).toBe('2');
+  });
+
+  describe('testing default functions', () => {
+    it('value from getRandomColor should have correct length', () => {
+      const color = Phone.defaultProps.getRandomColor();
+
+      expect(color.length).toBe(7);
+    });
+    it('value from getRandomColor should start with #', () => {
+      const color = Phone.defaultProps.getRandomColor();
+
+      expect(color.split('')[0]).toBe('#');
+    });
+    it('add should call correct functionms', () => {
+      const setValue = jest.fn();
+      const onType = jest.fn();
+      const setColor = jest.fn();
+      const getRandomColor = jest.fn(() => '#cdcdcd');
+
+      Phone.defaultProps.add('2', {
+        value: '3',
+        setValue,
+        setColor,
+        onType,
+        getRandomColor,
+      });
+
+      expect(setValue).toHaveBeenLastCalledWith('32');
+      expect(onType).toHaveBeenLastCalledWith('32');
+      expect(getRandomColor).toHaveBeenCalled();
+      expect(setColor).toHaveBeenCalledWith('#cdcdcd');
+    });
   });
 });
